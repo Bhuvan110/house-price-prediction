7cb268d6198e5124eb09c0140eef5410708cb411
Bhuvan gk <bbhuvangk@gmail.com>
2026-06-06 16:04:06 +0530

    Delete boston_price.py

--- boston_price.py
+++
@@ -1,6 +0,0 @@
-import seaborn as sns
-import pandas as pd
-import matplotlib.pyplot as plt
-data=pd.read_csv('D:\Downloads\HousingData.csv')
-sns.displot(data.MEDV,kde=True,color='red')#MEDV:cost in 1000's
-plt.show()
